4255063fbac820eca0362f0df315230eddd095a2
Martijn Dorsman <mdorsman@flaire.nl>
2019-08-03 20:09:17 +0200

    Quote of the day

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,20 @@
   <v-app>
     <Navbar />
     <v-layout align-center justify-center column>
-      <p>{{ quote}}</p>
-      <v-btn @click="fetchQuote" color="primary">New quote</v-btn>
+      <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on }">
+          <v-btn color="red lighten-2" @click="fetchQuote" dark v-on="on">Click me</v-btn>
+        </template>
+        <v-card>
+          <v-card-title class="headline" primary-title>Quote of the day</v-card-title>
+          <v-card-text class="pt-4">{{ quote }}</v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false">Cool stuff</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-layout>
   </v-app>
 </template>
@@ -20,24 +32,24 @@ export default {
   data() {
     return {
       quote: "Loading...",
+      dialog: false,
       index: 0
     };
   },
   methods: {
     fetchQuote() {
+      const data = {
+        index: this.index
+      };
+      const config = {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      };
+
       this.different = false;
       axios
-        .post(
-          "https://inspire.martijnd.workers.dev",
-          {
-            index: this.index
-          },
-          {
-            headers: {
-              "Content-Type": "application/json"
-            }
-          }
-        )
+        .post("https://inspire.martijnd.workers.dev", data, config)
         .then(res => {
           this.quote = res.data.quote;
           this.index = res.data.index;
